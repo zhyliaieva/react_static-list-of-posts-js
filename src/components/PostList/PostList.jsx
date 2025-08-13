6f32c1131b2d5eb137c36db9deb0b30e01652892
Dina Zhyliaieva <din.zhyliaieva@gmail.com>
2025-08-13 15:35:28 +0300

    REACT Post List

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,12 @@
-export const PostList = () => <>Put the list here</>;
+import { PostInfo } from '../PostInfo/PostInfo';
+
+// PostList component to display a list of posts
+export const PostList =  ({ posts }) => (
+  <div className="PostList">
+    {posts.map(post => (
+      <p key={post.id} >
+        <PostInfo post={post} />
+      </p>
+    ))}
+  </div>
+);
